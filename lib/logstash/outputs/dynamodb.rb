e56c873c4bd908125f5550b7692dc004845bf435
Jason Pilz <jasonpilz@gmail.com>
2018-07-13 23:46:15 -0700

    First

---
+++ lib/logstash/outputs/dynamodb.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+require "logstash/outputs/base"
+require "logstash/namespace"
+
+# An dynamodb output that does nothing.
+class LogStash::Outputs::Dynamodb < LogStash::Outputs::Base
+  config_name "dynamodb"
+
+  public
+  def register
+  end # def register
+
+  public
+  def receive(event)
+    return "Event received"
+  end # def event
+end # class LogStash::Outputs::Dynamodb
